de0eee9efb82e376c09704df5642db4ff5259850
Coradin <anthony.coradin@fibermetrix.fr>
2023-09-16 18:45:24 +0200

    Remplissage des jours du mois précédent

--- myscript.js
+++ myscript.js
@@ -1,5 +1,32 @@
+let tagJours = document.getElementById("jours");
 
 
-let lastMayNumberDay = new Date(2023, 5, 0).getDate();
-let lastMayDay = new Date(2023, 5, 0).getDay();
+let date = new Date();
+let annéeEnCours = date.getFullYear();
+let moisEnCours = date.getMonth();
+let listJours = "";
 
+const genererCalendrier = () => {
+    //Mois actuel  
+    let premierJourMois = new Date(annéeEnCours, moisEnCours, 1).getDay();
+    let dernierJourNbrMois = new Date(annéeEnCours, moisEnCours + 1, 0).getDate();
+    let dernierJourMois = new Date(annéeEnCours, moisEnCours, dernierJourNbrMois).getDay();
+    
+    //Mois précédent
+    let dernierJourNbrMoisPrecedent = new Date(annéeEnCours, moisEnCours, 0).getDate();
+
+    //On construit les jours du mois précedent
+        //ici on fait -1 car notre semaine commence le lundi et non le dimanche
+    for(let i = premierJourMois - 1; i > 0; i--){
+        listJours += "<li>" + (dernierJourNbrMoisPrecedent - i + 1) + "</li>";
+    }
+
+
+    //On construit les jours du mois en cours
+
+
+    //On construit les jours du mois à venir
+
+    tagJours.innerHTML = listJours;
+}
+genererCalendrier();
