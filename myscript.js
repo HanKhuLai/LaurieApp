c4651280d03b77efe92c1ab363936eb12b77c265
Coradin <anthony.coradin@fibermetrix.fr>
2023-09-18 10:26:34 +0200

    Changer les mois et années

--- myscript.js
+++ myscript.js
@@ -3,39 +3,82 @@ let annéeSelec = document.getElementById("annéeSelec");
 let moisSelec = document.getElementById("moisSelec");
 
 let date = new Date();
-let annéeEnCours = date.getFullYear();
-let moisEnCours = date.getMonth();
-let listJours = "";
+let annéeAffichée = date.getFullYear();
+let moisAffiché = date.getMonth();
 
-const mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
+const mois = [
+        {Id:0, Name:"Janvier"}, 
+        {Id:1, Name:"Février"}, 
+        {Id:2, Name:"Mars"}, 
+        {Id:3, Name:"Avril"}, 
+        {Id:4, Name:"Mai"}, 
+        {Id:5, Name:"Juin"}, 
+        {Id:6, Name:"Juillet"}, 
+        {Id:7, Name:"Août"}, 
+        {Id:8, Name:"Septembre"}, 
+        {Id:9, Name:"Octobre"}, 
+        {Id:10, Name:"Novembre"},
+        {Id:11, Name:"Décembre"}
+    ]
 
 const genererCalendrier = () => {
+    let listJours = "";
     //Mois actuel  
-    let premierJourMois = new Date(annéeEnCours, moisEnCours, 1).getDay();
-    let dernierJourNbrMois = new Date(annéeEnCours, moisEnCours + 1, 0).getDate();
-    let dernierJourMois = new Date(annéeEnCours, moisEnCours, dernierJourNbrMois).getDay();
-    
+    let premierJourMois = new Date(annéeAffichée, moisAffiché, 1).getDay();
+    if(premierJourMois == 0)
+        premierJourMois = 7;
+    let dernierJourNbrMois = new Date(annéeAffichée, moisAffiché + 1, 0).getDate();
+    let dernierJourMois = new Date(annéeAffichée, moisAffiché, dernierJourNbrMois).getDay();
+    if(dernierJourMois == 0)
+        dernierJourMois = 7;
     //Mois précédent
-    let dernierJourNbrMoisPrecedent = new Date(annéeEnCours, moisEnCours, 0).getDate();
+    let dernierJourNbrMoisPrecedent = new Date(annéeAffichée, moisAffiché, 0).getDate();
 
     //On construit les jours du mois précedent
         //ici on fait -1 car notre semaine commence le lundi et non le dimanche
-    for(let i = premierJourMois - 1; i > 0; i--){
-        listJours += '<li class="inactive">' + (dernierJourNbrMoisPrecedent - i + 1) + "</li>";
+    for(let i = premierJourMois; i > 1; i--){
+        listJours += '<li class="inactive">' + (dernierJourNbrMoisPrecedent - i + 2) + "</li>";
     }
 
     //On construit les jours du mois en cours
     for(let i = 1; i <= dernierJourNbrMois; i++){
-        listJours += "<li>" + i + "</li>";
+        listJours += '<li onClick="changeBGC(this)">' + i + "</li>";
     }
 
     //On construit les jours du mois à venir
-    for(let i = dernierJourMois + 1; i <= 7; i++){
-        listJours += '<li class="inactive">' + (i - dernierJourMois) + "</li>";
+    for(let i = dernierJourMois; i < 7; i++){
+        listJours += '<li class="inactive">' + (i - dernierJourMois + 1) + "</li>";
     }
 
-    annéeSelec.innerText = annéeEnCours
-    moisSelec.innerText = mois[moisEnCours]
+    annéeSelec.innerText = annéeAffichée;
+    moisSelec.innerText = mois.find(x=> x.Id === moisAffiché).Name;
     tagJours.innerHTML = listJours;
 }
 genererCalendrier();
+
+function changeBGC(elem){
+    if(elem.style.backgroundColor == "")
+        elem.style.backgroundColor = "darkgrey";
+    else
+        elem.style.backgroundColor = "";
+}
+
+function prevYear(elem){
+    annéeAffichée = parseInt(annéeSelec.innerText) - 1;
+    genererCalendrier();
+}
+
+function nextYear(elem){
+    annéeAffichée = parseInt(annéeSelec.innerText) + 1;
+    genererCalendrier();
+}
+
+function prevMonth(elem){
+    moisAffiché = mois.find(x=> x.Name === moisSelec.innerText).Id - 1;
+    genererCalendrier();
+}
+
+function nextMonth(elem){
+    moisAffiché = mois.find(x=> x.Name === moisSelec.innerText).Id + 1
+    genererCalendrier();
+}
